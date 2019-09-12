8547c226fa751bd0383117b23132484287c2e7b5
sajag16642 <sajag16642gmail.com>
2019-09-13 00:10:21 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -87,8 +87,6 @@ export class AppComponent implements OnInit {
       }
       this.isLogin='true';
       document.getElementById('navbar').style.zIndex='200';
-      document.body.style.backgroundImage="url(assets/bgicon.png)";
-      document.body.style.backgroundSize="cover";
     }
     else
     {
